1b858bc307cf097d5db32422988b1afd5cb1a5e5
Thomas Ranque <tranque@free.fr>
2021-10-09 18:19:47 +0200

    answers

--- 03-transforming-promises/answers.js
+++ 03-transforming-promises/answers.js
@@ -1,48 +1,73 @@
 /**
- * 
+ *
  * EXERCISE 1
- * 
- * @param {*} promise 
- * @param {*} transformer 
+ *
+ * @param {*} promise
+ * @param {*} transformer
  * @returns {Promise}
  */
-function mapPromise(promise, transformer){
+function mapPromise(promise, transformer) {
   return new Promise((resolve, reject) => {
     /* IMPLEMENT ME!! */
+    promise
+      .then((result) => {
+        resolve(transformer(result));
+      })
+      .catch((error) => reject(error));
   });
 }
 
 /**
- * 
+ *
  * EXERCISE 2
- * 
- * @param {Promise<number | string>} numberPromise 
+ *
+ * @param {Promise<number | string>} numberPromise
  * @returns {Promise<number>}
  */
-function squarePromise(numberPromise){
+function squarePromise(numberPromise) {
   return numberPromise
-    .then(/* IMPLEMENT ME! */);
+    .then((data) => {
+      return new Promise((resolve, reject) => {
+        if (typeof data === 'number') {
+          resolve(Math.pow(data, 2));
+        } else if (!isNaN(data)) {
+          resolve(Math.pow(+data, 2));
+        } else {
+          reject(`Cannot convert '${data}' to a number!`);
+        }
+      });
+    })
+    .catch((error) => Promise.reject(error));
+
 }
 
 /**
  * EXERCISE 3
- * 
- * @param {Promise<number | string>} numberPromise 
+ *
+ * @param {Promise<number | string>} numberPromise
  * @returns {Promise<number>}
  */
-function squarePromiseOrZero(promise){
+function squarePromiseOrZero(promise) {
   return squarePromise(promise)
-    .catch(/* IMPLEMENT ME! */);
+    .catch(() => Promise.resolve(0));
 }
 
 /**
  * EXERCISE 4
- * 
- * @param {Promise} promise 
+ *
+ * @param {Promise} promise
  * @returns {Promise}
  */
-function switcheroo(promise){
-  return promise.then(/* IMPLEMENT ME */);
+function switcheroo(promise) {
+  return promise.then(
+    /* IMPLEMENT ME */
+    (success) => {
+      throw (success);
+    },
+    (error) => {
+      return (error);
+    }
+  );
 }
 
 /**
--- 04-chaining-promises/answers.js
+++ 04-chaining-promises/answers.js
@@ -1,38 +1,61 @@
 /**
- * 
+ *
  * EXERCISE 1
- * 
- * @param {Promise} promise 
- * @param {function} asyncTransformer 
+ *
+ * @param {Promise} promise
+ * @param {function} asyncTransformer
  */
-function flatMapPromise(promise, asyncTransformer){
+function flatMapPromise(promise, asyncTransformer) {
   return new Promise((resolve, reject) => {
     promise
-      .then(/* IMPLEMENT ME! */);
+      .then((data) => {
+        resolve(asyncTransformer(data));
+      })
+      .catch((error) => {
+        reject(error);
+      });
   });
 }
 
 /**
- * 
+ *
  * EXERCISE 2
- * 
- * @param {Promise} firstPromise 
- * @param {function} slowAsyncProcess 
+ *
+ * @param {Promise} firstPromise
+ * @param {function} slowAsyncProcess
  */
-function chainTwoAsyncProcesses(firstPromise, slowAsyncProcess){
-  return firstPromise.then(/* IMPLEMENT ME! */);
+function chainTwoAsyncProcesses(firstPromise, slowAsyncProcess) {
+  return firstPromise.then((data) => {
+    return new Promise((res, rej) => {
+      slowAsyncProcess(data)
+        .then((value) => { res(value); })
+        .catch((error) => { rej(error); });
+    })
+  });
 }
 
 /**
- * 
+ *
  * EXERCISE 3
- * 
- * @param {function} getUserById 
- * @param {function} getOrganizationById 
+ *
+ * @param {function} getUserById
+ * @param {function} getOrganizationById
  */
-function makeGetUserByIdWithOrganization(getUserById, getOrganizationById){
-  return function getUserByIdWithOrganization(userId){
-    /* IMPLEMENT ME! */
+function makeGetUserByIdWithOrganization(getUserById, getOrganizationById) {
+  return function getUserByIdWithOrganization(userId) {
+    return getUserById(userId)
+      .then((data) => {
+        return new Promise((resolve, reject) => {
+          let orgId = data['organizationId'];
+          getOrganizationById(orgId)
+            .then((organization) => {
+              data['organization'] = organization;
+              resolve(data);
+            })
+            .catch(() => reject());
+        });
+      })
+      .catch((error) => { Promise.reject(error); });
   };
 }
 
